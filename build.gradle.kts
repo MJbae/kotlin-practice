bd5066535d28a1bb48283543b23665ea088a3fa9
MJbae <bmj5208@gmail.com>
2022-11-29 18:02:47 +0900

    default method 컴파일을 위한 관련 설정 추가
    참고: https://zorba91.tistory.com/340

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
+		freeCompilerArgs = listOf("-Xjsr305=strict", "-Xjvm-default=all")
 		jvmTarget = "11"
 	}
 }
