574ba4f2c2ed375cebc960b152fc6b0704cea90a
MJbae <bmj5208@gmail.com>
2022-11-29 15:39:09 +0900

    add post method in controller

--- src/main/kotlin/com/httpapiserver/HomeController.kt
+++ src/main/kotlin/com/httpapiserver/HomeController.kt
@@ -1,8 +1,9 @@
 package com.httpapiserver
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -10,9 +11,8 @@ import org.springframework.web.bind.annotation.RestController
 class HomeController {
 
     @GetMapping
-    fun home(@RequestParam(value = "name") name: String?) = listOf(
-        Message("1", "hello"),
-        Message("2", "$name"),
-        Message("3", "who u r")
-    )
+    fun home(): List<Message> = messageService.loadMessages()
+
+    @PostMapping
+    fun post(@RequestBody message: Message) = messageService.createMessage(message)
 }
